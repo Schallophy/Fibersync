e8b8f166fc46ffb15c1663b76a5d8fec659944c1
Schallophy <1317146226@qq.com>
2025-12-25 10:54:12 +0800

    Fix: Send PlayerAbilitiesS2CPacket when player returns from Limbo to restore block breaking ability

--- src/main/java/com/hadroncfy/fibersync/mixin/MixinPlayerManager.java
+++ src/main/java/com/hadroncfy/fibersync/mixin/MixinPlayerManager.java
@@ -16,8 +16,10 @@ import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 import net.minecraft.advancement.PlayerAdvancementTracker;
+import net.minecraft.entity.player.PlayerAbilities;
 import net.minecraft.network.ClientConnection;
 import net.minecraft.network.packet.s2c.play.CommonPlayerSpawnInfo;
+import net.minecraft.network.packet.s2c.play.PlayerAbilitiesS2CPacket;
 import net.minecraft.network.packet.s2c.play.PlayerRespawnS2CPacket;
 import net.minecraft.server.MinecraftServer;
 import net.minecraft.server.PlayerManager;
@@ -61,6 +63,10 @@ public class MixinPlayerManager implements IPlayerManager {
             
             connection.send(new PlayerRespawnS2CPacket(spawnInfo, (byte) 0));
             connection.send(new PlayerRespawnS2CPacket(spawnInfo, (byte) 0));
+            
+            // Send player abilities to ensure client has correct ability state
+            PlayerAbilities abilities = player.getAbilities();
+            connection.send(new PlayerAbilitiesS2CPacket(abilities));
         }
         var progress_bar = ((IServer) this.server).getBackupCommandContext(null).progress_bar.get();
         if (progress_bar != null) {
